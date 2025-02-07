2e00e88e20cbd4a099abadaf50f3a1772b3fd5c8
Anushi Rajput <anushi4849@gmail.com>
2025-02-07 15:35:19 +0530

    modified server.js with vercel link in cors middleware

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: ['http://localhost:5173', 'http://localhost:5174', 'http://localhost:5713', 'http://localhost:5714'],
+    origin: ['http://localhost:5713', 'http://localhost:5714', 'https://fusion-fest-hack-guide-x-project.vercel.app/'],
     methods: ['GET', 'POST'],
   },
 });
