d3233ac579cf1a62301637a8b81786734cc10d1f
Anushi Rajput <anushi4849@gmail.com>
2025-02-11 10:50:36 +0530

    fixed vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,15 +2,14 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    react(),tailwindcss(),
+    react(), tailwindcss(),
   ],
   server: {
     proxy: {
-      '/api': '${process.env.REACT_APP_BACKEND_URL}',
+      '/api': process.env.REACT_APP_BACKEND_URL,
     },
   },
 })
