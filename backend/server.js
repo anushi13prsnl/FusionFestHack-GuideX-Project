c232b789aadddcb3509bc9e1796e4b23a079c896
Anushi Rajput <anushi4849@gmail.com>
2025-02-11 09:45:12 +0530

    updated server.js for deployment

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,7 @@ const io = new Server(server, {
 
 app.use(express.json());
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:5174', 'http://localhost:5713', 'http://localhost:5714'],
+  origin: ['http://localhost:5173', 'http://localhost:5174', 'http://localhost:5713', 'http://localhost:5714', 'https://fusion-fest-hack-guide-x-project.vercel.app/'],
   methods: 'GET,POST,PUT,DELETE',
   credentials: true,
 }));
